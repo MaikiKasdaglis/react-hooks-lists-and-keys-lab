58fdb44c4bb26d1e2a991bae3bcbe19e7a0bb712
Michael Kasdaglis <maikikasdaglis@gmail.com>
2023-07-01 14:31:33 -0600

    completes lab

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,17 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+
+ const newDeliverable = technologies.map((tech) => {
+  // console.log(`tech from technologies`, tech)
+  return <span key={tech}>{tech}</span>
+ })
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {newDeliverable}
       </div>
     </div>
   );
